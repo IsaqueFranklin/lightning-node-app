04bd92255bc25cea9cbae7a3835b51847d479633
Isaque Franklin <isaquefrankli@gmail.com>
2024-11-02 22:23:13 -0300

    User model stuff.

--- src/utils/database/models/User.ts
+++ src/utils/database/models/User.ts
@@ -2,4 +2,20 @@ import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
     username: {type: String, required: true},
-})
+    profilePhotos: {
+        type: String,
+        default: function () {
+            return `https://secure.gravatar.com/${this._id}?s-98&d-identicon`;
+        }
+    }
+});
+
+UserSchema.set("toJSON", { getters: true });
+
+UserSchema.options.toJSON.transform = (doc:any, ret:any) => {
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+};
+
+const User = 
