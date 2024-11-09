886e10e57d9a6dc7b09e81581f22e45e25fbdc34
Isaque Franklin <isaquefrankli@gmail.com>
2024-11-09 16:19:21 -0300

    Post Schema.

--- src/utils/database/models/Posts.ts
+++ src/utils/database/models/Posts.ts
@@ -1,1 +1,18 @@
 import { Schema, model, models } from "mongoose";
+
+const PostSchema:any = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    author: { type: String, required: true },
+})
+
+PostSchema.set("toJSON", { getters: true });
+
+PostSchema.options.toJSON.transform = (doc:any, ret:any) => {
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+};
+
+const User = models.User || model("User", PostSchema);
+export default User;
