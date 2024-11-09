b034e69338810cec91c7354a3ec738150c366f3e
Isaque Franklin <isaquefrankli@gmail.com>
2024-11-09 17:24:45 -0300

    Adding sessionProvider

--- src/utils/nextAuth/authOptions.ts
+++ src/utils/nextAuth/authOptions.ts
@@ -2,10 +2,16 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import { AuthOptions } from "next-auth";
 import clientPromise from "./db-config";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
     providers: [
-        GoogleProvider({
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        })
+
+        /* GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
             authorization: {
@@ -15,8 +21,13 @@ export const authOptions = {
                 response_type: "code"
               }
             }
-        }),
+        }), */
     ],
+    callbacks: {
+        async session({ session }) {
+            return session;
+        }
+    },
     secret: process.env.JWT_SECRET_KEY as string,
     adapter: MongoDBAdapter(clientPromise),
 } as AuthOptions
